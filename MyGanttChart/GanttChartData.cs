18fcdaa8327c09d4d79f1f07735e3531cd5d1710
LesleyChang <lesleychang1024@gmail.com>
2018-10-09 17:48:54 +0800

    Initial commit

---
+++ MyGanttChart/GanttChartData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGanttChart
+{
+    public class GanttChartData
+    {
+        public GanttChartData()
+        {
+            //RowGroups = new ObservableCollection<GanttRowGroup>();
+            TimeLines = new ObservableCollection<TimeLine>();
+        }
+        //public ObservableCollection<GanttRowGroup> RowGroups { get; set; }
+        public ObservableCollection<TimeLine> TimeLines { get; set; }
+        public DateTime MinDate { get; set; }
+        public DateTime MaxDate { get; set; }
+    }
+}
---
+++ MyGanttChart/UserControl_Gantt.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyGanttChart
+{
+    /// <summary>
+    /// UserControl_Gantt.xaml 的互動邏輯
+    /// </summary>
+    public partial class UserControl_Gantt : Window
+    {
+        public UserControl_Gantt()
+        {
+            InitializeComponent();
+            DataContext = this;
+            SelectionPeriod = new Period();
+            
+        }
+
+        
+
+
+
+
+
+
+
+        private GanttChartData ganttChartData = new GanttChartData();
+        public GanttChartData GanttData { get { return ganttChartData; } }
+        private TimeLine gridLineTimeLine;
+        private ObservableCollection<TimeLine> gridLineTimeLines = new ObservableCollection<TimeLine>();
+        public ObservableCollection<TimeLine> GridLineTimeLine { get { return gridLineTimeLines; } }
+        public Period SelectionPeriod { get; private set; }
+    }
+}
